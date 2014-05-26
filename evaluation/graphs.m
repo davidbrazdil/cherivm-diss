c605f1eba9e09300df32f1477ccd96c8a1bd2f92
David Brazdil <david.brazdil@gmail.com>
2014-05-26 18:03:45 +0100

    Evaluation: updated overhead graph

--- evaluation/graphs.m
+++ evaluation/graphs.m
@@ -1,15 +1,15 @@
 Y =    [ 100.00  99.38  289.96          % get*Size
          100.00  87.82  156.89          % initIDs
          100.00 100.25  100.19          % generateKeys
-         100.00   0.00  116.61          % static webpage
-         100.00   0.00  117.64          % file server
+         100.00 100.12  116.61          % static webpage
+         100.00 100.31  117.64          % file server
        ];
        
 errY = [   6.94   6.58   32.12          % get*Size
           35.81  31.11   27.95          % initIDs
            7.17   3.91    3.92          % generateKeys
-           9.33   0.00   10.18          % static webpage
-           1.85   0.00    2.26          % file server
+           9.33   8.34   10.18          % static webpage
+           1.85   2.03    2.26          % file server
        ];
 
 xlab = { 'getPublicKeySize'
