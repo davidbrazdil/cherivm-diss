83461f404d8063870f4df7c88a5a7f6c1f7fd10f
David Brazdil <david.brazdil@gmail.com>
2014-05-29 11:11:12 +0100

    Evaluation: matlab graphs

--- evaluation/graphs.m
+++ evaluation/graphs.m
@@ -1,31 +1,43 @@
+% Change default axes fonts.
+set(0,'DefaultAxesFontName', 'Arial')
+set(0,'DefaultAxesFontSize', 14)
+
+% Change default text fonts.
+set(0,'DefaultTextFontname', 'Arial')
+set(0,'DefaultTextFontSize', 14)
+
+% Change default plot line colours
+set(0,'DefaultAxesColorOrder',[255 116 0; 0 153 153; 0 204 0; 255 0 0] ./ 255);
+
 % MICROBENCHMARKS
 
 Y =    [ 100.00  99.38  289.96          % get*Size
          100.00  87.82  156.89          % initIDs
          100.00 100.25  100.19          % generateKeys
-         100.00 100.12  116.61          % static webpage
-         100.00 100.31  117.64          % file server
+         100.00 100.90  100.29          % encryptData
+         100.00 100.18   99.54          % decryptData
        ];
        
 errY = [   6.94   6.58   32.12          % get*Size
           35.81  31.11   27.95          % initIDs
            7.17   3.91    3.92          % generateKeys
-           9.33   8.34   10.18          % static webpage
-           1.85   2.03    2.26          % file server
+           1.29   1.44    0.44          % encryptData
+           1.47   1.32    1.29          % decryptData
        ];
 
 xlab = { 'getPublicKeySize'
          'initIDs'
          'generateKeys' 
-         'static webpage'
-         'file server'
+         'encryptData'
+         'decryptData'
        };
 
 ylab = 'running time diff / %';
 
-lege = { 'JNI with direct buffers' 'JNI without direct buffers' 'Qishr' };
+lege = { 'JamVM' 'JamVM (w/o direct buffers)' 'Qishr' };
 
-figure
+hfig = figure;
+set(hfig, 'Position', [0 0 1024 576]);
 colormap('summer');
 handle = barwitherr(errY, Y);
 grid on
@@ -39,6 +51,37 @@ xlim = get(gca, 'xlim');
 % plot(xlim, [100 100])
 
 
+% ACCESS CONTROL OVERHEAD
+
+Y =    [   1.70   1.05    0.23          % NULL
+           1.40   1.06    0.68          % Always grant
+           1.71   1.02    0.92          % 1 comparison
+           1.62   1.08    1.06          % 4 comparisons
+           1.62   1.08    1.26          % 8 comparisons
+           1.63   1.11    1.43          % 16 comparisons
+       ];
+yrange = [6:-1:1];
+       
+xlab = { '16 comparisons' 
+         '8 comparisons' 
+         '4 comparisons' 
+         '1 comparison' 
+         'Always grant'
+         'NULL'
+       };
+
+ylab = 'invocation time / ms';
+
+lege = { 'JamVM' 'Qishr JNI overhead' 'Security Manager overhead' };
+
+hfig = figure;
+set(hfig, 'Position', [0 0 1024 576]);
+colormap('summer');
+handle = barh(yrange, Y, 'stack');
+grid on
+set(gca, 'YTickLabel', xlab, 'XTick', 1:numel(xlab))
+xlabel(ylab);
+legend(handle, lege);
 
 % MATRIX SQUARING
 
@@ -56,11 +99,19 @@ Y = [   20051730	  22965120	 29275540	 132895680
 
 lege = { 'JamVM Java' 'Qishr Java' 'JamVM JNI' 'Qishr JNI' };
 
+xlab = 'matrix size';
+ylab = 'time to square / s';
+
 xrange = 1:8;
 yrange = [0 2];
 
-figure;
-handle = plot(xrange, Y, 'LineSmoothing', 'on');
+hfig = figure;
+set(hfig, 'Position', [0 0 1024 576]);
+colormap('summer');
+handle = plot(xrange, Y, 'LineWidth',2);
+colormap('summer');
 legend(handle, lege);
 ylim(yrange);
+xlabel(xlab);
+ylabel(ylab);
 grid on;
